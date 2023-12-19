cee2623178f4b164ef943254c85a84445d81982f
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-12-19 01:17:34 -0300

    modificados:     src/main/java/com/spring/security/SpringSecurity/Entity/User.java

--- src/main/java/com/spring/security/SpringSecurity/Repository/UserRepository.java
+++ src/main/java/com/spring/security/SpringSecurity/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.spring.security.SpringSecurity.Repository;
 
 import com.spring.security.SpringSecurity.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
