808fd37d821b32fe8be37a194d3d6455bb938317
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-12-18 21:04:59 -0300

    FEAT: Implementation a lamda function in the authorize request, because this method presents more decoupled code

--- src/main/java/com/spring/security/SpringSecurity/config/Config.java
+++ src/main/java/com/spring/security/SpringSecurity/config/Config.java
@@ -14,14 +14,29 @@ import javax.management.MXBean;
 @EnableWebSecurity
 public class Config {
 
-    @Bean
+   /** @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-      /*  /* http.csrf().disable(); Disable default springSecurity security, is recomended when it is not interactuated with direct form*/
-        /*Indicates, witch are the end-point that need authorization */
-        http.authorizeHttpRequests().requestMatchers("/swagger-ui/**").permitAll(); /*The authentication is not necesseary in this end-point*/
-        http.authorizeHttpRequests().anyRequest().authenticated();/*But, other end-pont, the authentication is necesary*/
-        http.formLogin().permitAll();/*Enable form for all user*/
+      /*  /* http.csrf().disable(); Disable default springSecurity security, is recomended when it is not interactuated with direct form
+        /*Indicates, witch are the end-point that need authorization
+        http.authorizeHttpRequests().requestMatchers("/swagger-ui/**").permitAll(); /*The authentication is not necesseary in this end-point
+        http.authorizeHttpRequests().anyRequest().authenticated();/*But, other end-pont, the authentication is necesary
+        http.formLogin().permitAll();/*Enable form for all user
+        return http.build();
+    }*/
+
+    /**
+     * All endponits that have relation with /swagger-ui, the authentication it is not required
+     * but, other url, the authentication is required for all users
+     * */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+        ).formLogin()
+                .permitAll(); /** All endpoints before the success login, the authentication is required */
+
         return http.build();
     }
 }
