1491a96b88ad43166f6f4301edde5ae67a34db0e
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-12-19 02:04:50 -0300

    FEAT: USER REPOSITORY CLASS IMPLEMENT

--- src/main/java/com/spring/security/SpringSecurity/Repository/UserRepository.java
+++ src/main/java/com/spring/security/SpringSecurity/Repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.spring.security.SpringSecurity.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+import java.util.List;
 
+public interface UserRepository extends JpaRepository<User,Integer> {
+    @Query(value = "SELECT * FROM user u WHERE user.username = :username",nativeQuery = true)
+    public User findUserByUserName(String username);
 }
