2fafa84d937867efcdaa16dbc2aa6d9395bfb3be
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-12-18 22:20:35 -0300

    FEAT: Implement userDetailsService in a future update

---
+++ src/main/java/com/spring/security/SpringSecurity/UserDetailsServiceImplements.java
@@ -0,0 +1,12 @@
+package com.spring.security.SpringSecurity;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImplements implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
