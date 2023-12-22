8be69a70645a6fe423e6b8a4ca4d07bdad413766
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-12-22 17:44:56 -0300

    FEAT: IMPLEMENT USER SESSION ENPOINT MONITORING AND TEST

--- src/main/java/com/spring/security/SpringSecurity/Controller/Controller.java
+++ src/main/java/com/spring/security/SpringSecurity/Controller/Controller.java
@@ -1,2 +1,55 @@
-package com.spring.security.SpringSecurity.Controller;public class Controller {
+package com.spring.security.SpringSecurity.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("v1")
+public class Controller {
+
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
+    @GetMapping("/hola")
+    public String index(){
+        return "hola mundo";
+    }
+
+    @GetMapping("/DetailsSession")
+    public ResponseEntity<?> getUsersDetails(){
+
+        String sessionId = null;
+        User userObject = null;
+
+        /**RETURN ALL USER OF THE SESSION*/
+        List<Object> sessions = this.sessionRegistry.getAllPrincipals();
+        for(Object session : sessions){
+            if(session instanceof User){
+                /**GIVE USER*/
+                userObject = (User) session;
+            }
+            /**RETURN ALL SESSION INFORMATION*/
+            List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(userObject,false);
+            for(SessionInformation sessionI : sessionInformations){
+               /** GIVE THE ID OF THE USER*/
+               sessionId = sessionI.getSessionId();
+            }
+        }
+        Map<String,Object> response = new HashMap<>();
+        response.put("response","hello word");
+        response.put("sessionId",sessionId);
+        response.put("session user",userObject);
+
+        return ResponseEntity.ok(response);
+    }
 }
