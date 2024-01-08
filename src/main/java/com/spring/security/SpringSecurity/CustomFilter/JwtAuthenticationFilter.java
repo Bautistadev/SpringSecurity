a3e0a78f5781216dc652503db3dd8c6487317d80
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-01-08 16:26:49 -0300

    borrados:        src/main/java/com/spring/security/SpringSecurity/Controller/homeController.java

---
+++ src/main/java/com/spring/security/SpringSecurity/CustomFilter/JwtAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package com.spring.security.SpringSecurity.CustomFilter;
+
+import com.spring.security.SpringSecurity.Service.JWTUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public class CustomFilter  {
+
+
+
+}
---
+++ src/main/java/com/spring/security/SpringSecurity/CustomFilter/JwtAuthorizationFilter.java
@@ -0,0 +1,20 @@
+package com.spring.security.SpringSecurity.CustomFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+/**
+ *
+ * AFTER AUTHENTICATION, IS NECESSARY DEFINE THEN AUTHORIZATION
+ *
+ * */
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
